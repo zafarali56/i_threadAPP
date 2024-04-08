6300023c5fbca066291ebd0cc403229619b77796
zafar ali <zafaralyjamali722@gmail.com>
2024-04-08 12:28:05 +0500

    Initial Commit

---
+++ iThreads/iThreadsApp.swift
@@ -0,0 +1,17 @@
+//
+//  iThreadsApp.swift
+//  iThreads
+//
+//  Created by Zafar Ali on 08/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct iThreadsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
